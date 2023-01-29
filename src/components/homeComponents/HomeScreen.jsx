4b80df1dbe2cd2f0747313667b03a512116d203e
Paul Okoh <pauldokoh@gmail.com>
2023-01-28 18:49:06 -0600

    initial commit

---
+++ src/components/homeComponents/HomeScreen.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import axios  from "axios"
+import AdBanner from './AdBanner'
+
+const HomeScreen = () => { 
+  const [recipes, setRecipes] = useState([]) 
+
+  const getRecipes = () => {
+    axios
+    .get("https://recipes.devmountain.com/recipes")
+    .then((res)=> {
+      setRecipes(res.data)
+      console.log(res.data)
+    })
+  }
+
+  useEffect(()=> {
+    getRecipes()
+  },[])
+
+ 
+  return (
+    <div>
+      <AdBanner />
+      {/* Much code from Part 2 will be placed around here. Do your best! */}
+    </div>
+  )
+}
+
+
+
+
+export default HomeScreen
